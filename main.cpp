c7f02c5022d47c370ec0f10702a85b78ebb41b82
b57zheng <b57zheng@uwaterloo.ca>
2023-01-23 11:06:13 -0500

    main_program_update

--- main.cpp
+++ main.cpp
@@ -14,18 +14,6 @@
 #ifndef MARMOSET_TESTING
 unsigned int Transaction::assigned_trans_id = 0;
 int main() {
-  // Create a new variable called 'test_transaction' with type 'Transaction'
-  // The objective programming forced to have all the information to proceed {"VGRO", 15, 2, 2020, true, 300, 9900}
-  // Transaction test_transaction {"VGRO", 15, 2, 2020, true, 300, 9900};
-  // // Make it dynamic.
-  // Transaction * p_transaction { new Transaction {"VGRO", 15, 2, 2020, false, 200, 9900} };
-  // Transaction test1_transaction {"VGRO", 15, 2, 2021, true, 300, 9900};
-  // if (test_transaction < test1_transaction)
-  // {
-  //   std::cout << "less";
-  // }
-
-
   History trans_history{};
   //std::cout << "Hello" << std::endl;
   trans_history.read_history();
